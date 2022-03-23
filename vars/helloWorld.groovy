9886ae28afb8df5c5312f44508c6ae7e6b0318c3
stark985 <34186590+Kama001@users.noreply.github.com>
2022-03-23 14:18:45 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,11 +1,14 @@
 // def call(Map config = [:]) {
 //     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
 // }
-void call(List<String> processes) {
-    List<String> cmds = []
-    processes.each { process ->
-            cmds.add("tasklist | find /i \"${process}\" && taskkill /im ${process} /F")
-    }
-    cmds.join('\n')
-    echo "$cmds"
+// void call(List<String> processes) {
+//     List<String> cmds = []
+//     processes.each { process ->
+//             cmds.add("tasklist | find /i \"${process}\" && taskkill /im ${process} /F")
+//     }
+//     cmds.join('\n')
+//     echo "$cmds"
+// }
+void call(String input) {
+    node = path.join([var/lib/jenkins/, "${input}.json"])
 }
