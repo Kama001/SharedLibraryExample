835fe1a9835b0ec5f3dd76fbc52efc45dc2ca016
stark985 <34186590+Kama001@users.noreply.github.com>
2022-03-23 12:36:38 +0530

    Update doTask.groovy

--- vars/doTask.groovy
+++ vars/doTask.groovy
@@ -1,13 +1,16 @@
-void call(def task, Map settings) {
-  pipeline {
-    agent any 
-    stages {
-      stage('Check') {
-        steps {
-          echo "in doTask file"
-          echo "${settings.Region.asia}"
-        }
-      }
-    }
-  }
+// void call(def task, Map settings) {
+//   pipeline {
+//     agent any 
+//     stages {
+//       stage('Check') {
+//         steps {
+//           echo "in doTask file"
+//           echo "${settings.Region.asia}"
+//         }
+//       }
+//     }
+//   }
+// }
+void call() {
+  echo "helloWorld"
 }
