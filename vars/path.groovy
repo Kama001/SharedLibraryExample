bc5df6aa1ebd7aafa32a7dd9cb15d50ad46ae368
stark985 <34186590+Kama001@users.noreply.github.com>
2022-04-11 12:25:52 +0530

    Update path.groovy

--- vars/path.groovy
+++ vars/path.groovy
@@ -7,3 +7,16 @@ String normalize(String path) {
     echo "$path"
     return path.replace('/', '\\')
 }
+int m = grid.size();
+    int n = grid[0].size();
+    vector<vector<int>> res(m, vector<int>(n,0));
+    int total = m * n;
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            int indexToCopy = ((i*n)+j) + (k%(total);
+            if(indexToCopy >= total)
+                indexToCopy %= total;
+            res[(indexToCopy/m)][(indexToCopy%m)] = grid[i][j];
+        }
+    }
+    return res;
